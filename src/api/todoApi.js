74bdc3a45339caa39def03ba79a153edee1b075a
CHOHUNE <yuureru@gmail.com>
2024-04-16 16:56:22 +0900

    리드 컴포넌트, 네비게이션 관련 커스텀 훅 개발

--- src/api/todoApi.js
+++ src/api/todoApi.js
@@ -3,6 +3,8 @@ import axios from "axios";
 
 export const API_SERVER_HOST='http://localhost:8080'
 
+
+
 const prefix = `${API_SERVER_HOST}/api/todo` //
 
 export const getOne = async (tno) =>{
--- src/components/todo/ReadComponent.js
+++ src/components/todo/ReadComponent.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {getOne} from "../../api/todoApi";
+import useCustomMove from "../../hooks/useCustomMove";
 
 const initState ={
     tno:0,
@@ -13,6 +14,8 @@ function ReadComponent({tno}) {
 
     const [todo, setTodo] = useState(initState);
 
+    const {moveToList} = useCustomMove();
+
     useEffect(() => {
 
         getOne(tno).then(data=>{
@@ -23,8 +26,31 @@ function ReadComponent({tno}) {
     }, [tno]);
 
     return (
-        <div></div>
+        <div className={"border-2 border-sky-200 mt-10 m-2 p-4"}>
+            {makeDiv("TNO",todo.tno)}
+            {makeDiv("Writer",todo.writer)}
+            {makeDiv("Title",todo.title)}
+            {makeDiv("Due Date",todo.dueDate)}
+            {makeDiv("Complete",todo.complete ? "Completed" : "Not yet")}
+
+            <div className={"flex justify-end p-4"}>
+                <button type={"button"} className={"rounded p-4 m-2 text-xl text-white bg-blue-500"}
+                onClick={()=>moveToList()}>List</button>
+
+            </div>
+
+        </div>
     );
 }
+
+const makeDiv = (title, value) =>
+    <div className={"flex justify-centert"}>
+        <div className={"relative mb-4 flex w-full flex-wrap items-stretch"}>
+            <div className={"w-1/5 p-6 text-right font-bold"}> {title}</div>
+            <div className={"w-4/5 p-6 rounded-r border border-solid shadow-md"} >
+                {value}
+            </div>
+        </div>
+    </div>
 
 export default ReadComponent;
