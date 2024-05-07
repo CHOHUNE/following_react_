0409430e383875dc53c6304216f43e9b883ea405
CHOHUNE <yuureru@gmail.com>
2024-05-07 16:38:32 +0900

    FEAT : 리액트 사용자 정보 수정 social 로그인 시 연결 되는 ModifyComponent.js-> ModifyPage.js , 라우팅 설정

--- src/router/memberRouter.js
+++ src/router/memberRouter.js
@@ -6,6 +6,8 @@ const Login = lazy(() => import ("../pages/member/LoginPage"))
 const Logout = lazy(() => import ("../pages/member/LogoutPage"))
 const KakaoRedirect = lazy(() => import("../pages/member/KakaoRedirectPage"))
 
+const MemberModify = lazy(() => import("../pages/member/ModifyPage"))
+
 const memberRouter = () => {
     return [
 
@@ -18,6 +20,9 @@ const memberRouter = () => {
         }, {
             path: "kakao",
             element: <Suspense fallback={Loading}><KakaoRedirect/></Suspense>,
+        }, {
+            path: "modify",
+            element: <Suspense fallback={Loading}><MemberModify/></Suspense>
         }
     ];
 };
