6d4a5b3f97316fcc7be18e707729eaaf227c139b
CHOHUNE <yuureru@gmail.com>
2024-05-09 21:56:38 +0900

    FEAT : 장바구니용 컴포넌트 처리 FIX : redux 관련 오류 bug fix
    CartComponent ( 유저 이름과 아이템 갯수 표기),

--- src/slice/cartSlice.js
+++ src/slice/cartSlice.js
@@ -1,36 +1,23 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import {getCartItems} from "../api/cartApi";
+import {getCartItems, postChangeCart} from "../api/cartApi";
 
 export const getCartItemsAsync = createAsyncThunk('getCartItemsAsync', () => {
     return getCartItems()
 })
-
-export const postChangeCartAsync = createAsyncThunk(' postChangeCartAsync', (param) => {
-
-    return postChangeCartAsync(param)
-
+export const postChangeCartAsync = createAsyncThunk('postCartItemsAsync', (param) => {
+    return postChangeCart(param)
 })
-
 const initState = []
-
 const cartSlice = createSlice({
-    name: 'cartSlice',
-    initialState: initState,
-    extraReducers: (builder) => {
+    name: 'cartSlice', initialState: initState, extraReducers: (builder) => {
         builder.addCase(getCartItemsAsync.fulfilled, (state, action) => {
-
-            console.log("getCartItemAsync.fulfilled")
-            console.log(action.payload)
-
-            return action.payload // 진짜 데이터 : 장바구니 안의 아이템들
+            console.log("getCartItemsAsync fulfilled")
+            return action.payload
         })
             .addCase(postChangeCartAsync.fulfilled, (state, action) => {
-                console.log("postChangeCartAsync.fulfilled")
-
-                return action.payload // 변경 시에도 위와 동일하게 불러오기
-
+                console.log("postCartItemsAsync fulfilled")
+                return action.payload
             })
     }
 })
-
 export default cartSlice.reducer
--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import {configureStore} from '@reduxjs/toolkit';
 import loginSlice from './slice/loginSlice';
 import cartSlice from "./slice/cartSlice";
 
+
 export default configureStore({
     reducer: {
         "loginSlice": loginSlice,
