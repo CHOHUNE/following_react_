707bb71b8a81ea45e4a5bd5e2a1d4992b24f33ca
CHOHUNE <yuureru@gmail.com>
2024-05-28 12:43:35 +0900

    FIX : ETC

--- src/components/menus/CartComponent.js
+++ src/components/menus/CartComponent.js
@@ -1,7 +1,5 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import useCustomLogin from "../../hooks/useCustomLogin";
-import {useDispatch, useSelector} from "react-redux";
-import {getCartItemsAsync} from "../../slice/cartSlice";
 import useCustomCart from "../../hooks/useCustomCart";
 import CartItemComponent from "../cart/CartItemComponent";
 import {useRecoilValue} from "recoil";
